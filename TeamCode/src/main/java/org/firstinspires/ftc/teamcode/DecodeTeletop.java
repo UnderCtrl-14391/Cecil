05f83418200f0334f942e093da3c62bf17afb646
sofiarohr <sofiarohrlang@gmail.com>
2025-12-11 17:30:19 -0300

    feat: add Decode teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeletop.java
@@ -0,0 +1,215 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+@TeleOp
+public class DecodeTeletop extends LinearOpMode{
+    private DcMotor direita;
+    private DcMotor direitaTras;
+    private DcMotor esquerda;
+    private DcMotor esquerdaTras;
+    private DcMotor shooterD;
+    private DcMotor shooterE;
+    private CRServo index2;
+    private CRServo index1;
+    private DcMotor intakemotor;
+    double rapido;
+    float rx;
+    double denominator;
+    int p;
+    boolean b;
+    int targetVelocity;
+    boolean shooter;
+    boolean chuvadepogchamp;
+    boolean rb;
+    boolean lb;
+
+
+
+    private void runPIDShooter() {
+        double pidVelocityD;
+        double pidVelocityE;
+        pidVelocityE = p (((DcMotorEx)shooterE));targetVelocity( - targetVelocity);
+        pidVelocityD = p (((DcMotorEx)shooterD));targetVelocity( - targetVelocity);
+        ((DcMotorEx) shooterD).setVelocity(pidVelocityD);
+        ((DcMotorEx) shooterE).setVelocity(pidVelocityE);
+        {
+            if (gamepad2.x) {
+                targetVelocity = (1000 * 60) / 45;
+                runPIDShooter();
+            }
+            if (gamepad2.y) {
+                shooterD.setPower(0);
+                shooterE.setPower(0);
+                index2.setPower(0);
+                index1.setPower(0);
+            }
+            if (gamepad2.a) {
+                targetVelocity = (2600 * 60) / 45;
+                runPIDShooter();
+            }
+        }
+
+        boolean runOpMode;
+        {
+            boolean batente3;
+            boolean intake;
+            boolean rb;
+            boolean lb;
+            boolean chuvadepogchamp;
+
+            shooterD = hardwareMap.get(DcMotor.class, "shooterD");
+            shooterE = hardwareMap.get(DcMotor.class, "shooterE");
+            index2 = hardwareMap.get(CRServo.class, "index2");
+            index1 = hardwareMap.get(CRServo.class, "index1");
+            direitaTras=hardwareMap.get(DcMotor.class, "direitaTras");
+            direita = hardwareMap.get(DcMotor.class, "direita");
+            esquerdaTras = hardwareMap.get(DcMotor.class , "esquerdaTras");
+            esquerda = hardwareMap.get(DcMotor.class, "esquerda");
+            intakemotor = hardwareMap.get(DcMotor.class, "intake");
+
+            targetVelocity = (4000 * 60) / 45;
+            targetVelocity = (2800 * 60) / 45;
+            p = 1;
+            int f = 1;
+            b = false;
+            shooterD.setDirection(DcMotor.Direction.FORWARD);
+            shooterE.setDirection(DcMotor.Direction.REVERSE);
+            intake = false;
+            rb = false;
+            lb = false;
+            rapido = 0.8;
+            chuvadepogchamp = false;
+            waitForStart();
+            if (opModeIsActive()) {
+                // Put run blocks here.
+                while (opModeIsActive()) {
+                    // Put loop blocks here.
+
+                    noimu();
+                    intake();
+                    index1();
+                    index2();
+                    shooter();
+                    telemetry.update();
+                }
+            }
+        }
+
+        /**
+         * Describe this function...
+         */
+        int noimu;
+
+        {
+            float y;
+            double x;
+            float rx;
+            double denominator;
+
+            y = -gamepad1.left_stick_y;
+            x = gamepad1.left_stick_x * 1.1;
+            rx = gamepad1.right_stick_x;
+            esquerda.setPower((y + x + rx) * -0.9);
+            esquerdaTras.setPower(((y - x) + rx) * 0.9);
+            direita.setPower(((y - x) - rx) * 0.9);
+            direitaTras.setPower(((y + x) - rx) * 0.9);
+            denominator = JavaUtil.maxOfList(JavaUtil.createListWith(JavaUtil.sumOfList(JavaUtil.createListWith(Math.abs(y), Math.abs(x), Math.abs(rx))), 1));
+        }
+
+        /**
+         * Describe this function...
+         */
+        boolean intake2;
+        {
+            intakemotor.setPower(gamepad2.right_stick_y);
+        }
+
+        /**
+         * Describe this function...
+         */
+        boolean index;
+        {
+            index2.setPower(-gamepad2.left_stick_y);
+            index1.setPower(gamepad2.left_stick_y);
+            rb = false;
+            lb = false;
+            rapido = 0.9;
+            chuvadepogchamp = false;
+
+            waitForStart();
+
+            while (opModeIsActive()) {
+
+
+            }
+        }
+    }
+
+    private void noimu() {
+    }
+
+    private void intake() {
+    }
+
+    private void index2() {
+    }
+    private void index1() {
+    }
+
+    private void shooter() {
+    }
+
+    private void targetVelocity(int i) {
+    }
+
+    private double p(DcMotorEx shooterE) {
+        return 0;
+    }
+
+    private void imu(){
+        YawPitchRollAngles angle = null;
+        double yaw;
+        double x;
+        double y;
+        float rx;
+        double denominator;
+
+        if(0.4 <= gamepad1.right_trigger){
+            rapido = 0.2;
+        }
+        else {
+            rapido = 1;
+        }
+        yaw = angle.getYaw(AngleUnit.DEGREES);
+        y = gamepad1.left_stick_x * Math.sin(-yaw / 180 * Math.PI) * -1 * -1 + gamepad1.left_stick_y * Math.cos(-yaw / 180 * Math.PI) * -1;
+        x = gamepad1.left_stick_x * Math.cos(-yaw / 180 * Math.PI) + gamepad1.left_stick_y * Math.sin(-yaw/ 180 * Math.PI);
+        rx = gamepad1.right_stick_x;
+        denominator = JavaUtil.maxOfList(JavaUtil.createListWith(JavaUtil.sumOfList(JavaUtil.createListWith(Math.abs(y), Math.abs(x), Math.abs(rx))), 1));
+        direita.setPower((((y - x) - rx) / denominator) * rapido);
+        direitaTras.setPower((((y + x) - rx) / denominator) * rapido);
+        esquerda.setPower(((y + x + rx) / denominator) * rapido);
+        esquerdaTras.setPower(((y - x) + rx / denominator) * rapido);
+    }
+
+
+    public Object getVelocity(Object velocity) {
+        return velocity;
+    }
+
+    public void setVelocity(Object velocity) {
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
