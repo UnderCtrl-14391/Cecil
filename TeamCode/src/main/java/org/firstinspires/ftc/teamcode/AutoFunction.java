58d315291719fbf612d81e34039f630abf384780
TSLuiza <164081291+TSLuiza@users.noreply.github.com>
2025-12-12 15:42:08 -0300

    feat: add AutoFunction

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFunction.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class AutoFunction {
+    public DcMotor direita;
+    public DcMotor direitaTras;
+    public DcMotor esquerda;
+    public DcMotor esquerdaTras;
+    public DcMotor shooterD;
+    public DcMotor shooterE;
+    public CRServo index2;
+    public CRServo index1;
+    public DcMotor intakemotor;
+
+    public autonomous () {
+
+    }
+}
