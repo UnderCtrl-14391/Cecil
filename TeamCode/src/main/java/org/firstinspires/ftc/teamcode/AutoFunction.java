72c8aca8fccc31bd1b41ec33a27357155dad1a30
TSLuiza <164081291+TSLuiza@users.noreply.github.com>
2025-12-14 00:54:09 -0300

    feat: finish AutoFunction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFunction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFunction.java
@@ -4,21 +4,46 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class AutoFunction {
-    public DcMotor direita;
-    public DcMotor direitaTras;
-    public DcMotor esquerda;
-    public DcMotor esquerdaTras;
-    public DcMotor shooterD;
-    public DcMotor shooterE;
-    public CRServo index2;
-    public CRServo index1;
-    public DcMotor intakemotor;
+    private com.qualcomm.robotcore.hardware.HardwareMap hardwareMap;
+    public  DcMotor left;
+    public DcMotor right;
+    public DcMotor leftT;
+    public DcMotor rightT;
+    public DcMotor intake;
+    public DcMotor catapulta1;
+    public DcMotor catapulta2;
+    public DcMotor fat;
 
-    public void autonomous (int rightFront , int leftFront , int rightTarget , int leftTarget , int rightPower , int leftPower , int ShooterPower , int index , int intakePower) {
-        direita.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        direitaTras.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        esquerda.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        esquerdaTras.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        direita.setTargetPosition(rightFront);
+    public void HardwareMap(com.qualcomm.robotcore.hardware.HardwareMap hardwareMap) {
+        left = hardwareMap.get(DcMotor.class, "left");
+        right = hardwareMap.get(DcMotor.class, "right");
+        leftT = hardwareMap.get(DcMotor.class, "leftT");
+        rightT = hardwareMap.get(DcMotor.class, "rightT");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        catapulta1 = hardwareMap.get(DcMotor.class, "catapulta1");
+        catapulta2 = hardwareMap.get(DcMotor.class, "catapulta2");
+        fat = hardwareMap.get(DcMotor.class, "fat");
+    }
+
+    public void autonomous (int rightFront , int leftFront , int rightTarget , int leftTarget , int rightPower , int leftPower , int intakeP , int catapulta , int catapultaPower) {
+        //tração
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightT.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftT.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setTargetPosition(rightFront);
+        left.setTargetPosition(leftFront);
+        rightT.setTargetPosition(rightTarget);
+        leftT.setTargetPosition(leftTarget);
+        right.setPower(rightPower);
+        rightT.setPower(rightPower);
+        left.setPower(leftPower);
+        leftT.setPower(leftPower);
+        //mecanismos
+        intake.setPower(intakeP);
+        catapulta1.setTargetPosition(catapulta);
+        catapulta2.setTargetPosition(catapulta);
+        catapulta1.setPower(catapultaPower);
+        catapulta2.setPower(catapultaPower);
     }
 }
