75ad15b956a65bfc09c8a1da684a94d78dac046b
TSLuiza <164081291+TSLuiza@users.noreply.github.com>
2026-01-21 15:10:41 -0300

    fix: imu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imu.java
@@ -1,57 +1,51 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-public class imu extends OpMode{
+public class imu extends OpMode {
     double heading;
+    DecodeTeletop bench = new DecodeTeletop();
 
-      DecodeTeletop bench = new  DecodeTeletop ();
+    @Override
+    public void init() {
+        // Manually assigning the hardwareMap to the bench instance
+        // since DecodeTeletop is being used as a hardware class here.
+        bench.hardwareMap = hardwareMap;
+        
+        // Initializing motors and IMU
+        bench.right = hardwareMap.get(com.qualcomm.robotcore.hardware.DcMotor.class, "right");
+        bench.rightT = hardwareMap.get(com.qualcomm.robotcore.hardware.DcMotor.class, "rightT");
+        bench.left = hardwareMap.get(com.qualcomm.robotcore.hardware.DcMotor.class, "left");
+        bench.leftT = hardwareMap.get(com.qualcomm.robotcore.hardware.DcMotor.class, "leftT");
+        bench.imu = hardwareMap.get(com.qualcomm.robotcore.hardware.IMU.class, "imu");
 
-   @Override
-   public void init() {
-     bench.init(hardwareMap);
-   }
-
-   @Override
-   public void loop() {
-       heading = bench.getHeading(AngleUnit.DEGREES);
-       telemetry.addData("Heading", bench.getHeading(AngleUnit.DEGREES));
-
-       if (heading < 0.5 && heading > -0.5) {
-           bench.setRight(0.0);
-           bench.setLeft(0.0);
-           bench.setRight(0.0);
-           bench.setRight(0.0);
-       }
-   }
-           else if (heading <0.5){
-        bench.setRight(0.5);
-        bench.setLeft(0.5);
-        bench.setRight(0.5);
-        bench.setRight(0.5);
-       }
-           else{
-               bench.setRight(-0.5);
-        bench.setLeft(-0.5);
-        bench.setRight(-0.5);
-        bench.setRight(-0.5);
+        com.qualcomm.hardware.rev.RevHubOrientationOnRobot orientation = new com.qualcomm.hardware.rev.RevHubOrientationOnRobot(
+                com.qualcomm.hardware.rev.RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
+                com.qualcomm.hardware.rev.RevHubOrientationOnRobot.UsbFacingDirection.UP);
+        bench.imu.initialize(new com.qualcomm.robotcore.hardware.IMU.Parameters(orientation));
     }
 
-   }
+    @Override
+    public void loop() {
+        // Using getHeading() without arguments as defined in DecodeTeletop
+        heading = bench.getHeading();
+        telemetry.addData("Heading", heading);
 
+        if (heading < 0.5 && heading > -0.5) {
+            // Stop motors if heading is centered
+            bench.setRight(0.0);
+            bench.setLeft(0.0);
+        } else if (heading <= -0.5) {
+            // Heading is less than -0.5, make motors positive
+            bench.setRight(0.5);
+            bench.setLeft(0.5);
+        } else {
+            // Heading is greater than 0.5, make motors negative
+            bench.setRight(-0.5);
+            bench.setLeft(-0.5);
+        }
+        telemetry.update();
+    }
 }
-/*
-1.return the heading in radians
-2.when your heading is <0.5 && greater that -0.5, stop a motor.If its less than -0,5 makes your motor positive, and if its greater than 0.5 makes it negative.
- */
-
-
-
-
-
-
-
-
