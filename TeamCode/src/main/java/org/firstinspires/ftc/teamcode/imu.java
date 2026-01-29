d7b65363f174f8422918d7733ab6fc52494b6b57
Inácio <inaciotschaefer@gmail.com>
2026-01-29 20:27:58 -0300

    Create imu.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imu.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public abstract class imu extends LinearOpMode {
+
+}
