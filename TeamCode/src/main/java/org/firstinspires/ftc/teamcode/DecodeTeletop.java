730f29774c43942052404e34880c927200ca30b0
Inácio <inaciotschaefer@gmail.com>
2026-01-29 20:27:13 -0300

    fix tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeletop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeletop.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
