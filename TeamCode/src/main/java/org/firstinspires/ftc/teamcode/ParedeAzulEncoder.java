c2f338847a63b7b84bae8ae6653e39ebb5b59fcc
sofiarohr <sofiarohrlang@gmail.com>
2026-02-02 11:32:22 -0300

    fix paredeAzul

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParedeAzulEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParedeAzulEncoder.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.teamcode.TimerExample.runtime;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous
@@ -10,35 +11,36 @@ public class ParedeAzulEncoder extends AutoFunction {
 
         waitForStart();
         if (opModeIsActive()) {
-        autonomous(-1700,-1700,-1700,-1700,0.8,0.8,0, CatapultaModes.DOWN);
-        sleep(400);
+        autonomous(-1500,-1500,-1500,-1500,0.8,0.8,0, CatapultaModes.DOWN);
+        if (runtime.seconds() >= 2)
+
+        //sleep(100);
 
         autonomous(1,1,1,1,0.1,0.1,0, CatapultaModes.HOLD);
         sleep(3000);
 
-        autonomous(1700,1700,1700,1700,0.8,0.8,0,CatapultaModes.HOLD);
-        sleep(400);
-
-        autonomous(-500,-500,500,500,0.8,0.8,0, CatapultaModes.HOLD);
-        sleep(100);
-
-        autonomous(500,500,500,500,0.8,0.8,0, CatapultaModes.HOLD);
-        sleep(100);
+        autonomous(-2400,-2400,2400,2400,0.8,0.8,0,CatapultaModes.HOLD);
+        //sleep(100);
 
         autonomous(600,-600,600,-600,0.7,0.7,0, CatapultaModes.HOLD);
-        sleep(200);
+        //sleep(100);
 
-        autonomous(300,300,300,300,0.8,0.8,0, CatapultaModes.DOWN);
-        sleep(150);
+        autonomous(2000,2000,2000,2000,0.8,0.8,0, CatapultaModes.HOLD);
+        //sleep(200);
+
+        /*autonomous(300,300,300,300,0.8,0.8,0, CatapultaModes.DOWN);
+        //sleep(150);*/
 
         autonomous(0,0,0,0,0,0,0, CatapultaModes.UP);
-        sleep(100);
+        sleep(10);
 
         autonomous(-600,600,-600,600,0.7,0.7,0,CatapultaModes.DOWN);
-        sleep(100);
+        //sleep(100);
 
         autonomous(-2000,-2000,-2000,-2000,0.8,0.8,0,CatapultaModes.HOLD);
-        sleep(1200);
+        //sleep(1200);
+
+        autonomous(-5000, -5000, 5000, 5000, 0.8, 0.8, 0, CatapultaModes.HOLD);
 
         telemetry.update();
 
