b3cc197fd8cadf05cbc639249b6ce632c92bbd64
sofiarohr <sofiarohrlang@gmail.com>
2026-02-04 11:24:53 -0300

    fix driver hub full

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imu.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
-public abstract class imu extends LinearOpMode {
-
-}
