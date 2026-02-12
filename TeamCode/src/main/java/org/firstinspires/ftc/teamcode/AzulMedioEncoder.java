29f7a5912eeb66de7f6677b370f14f11b79e19a4
sofiarohr <sofiarohrlang@gmail.com>
2026-02-12 14:04:11 -0300

    fix auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AzulMedioEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AzulMedioEncoder.java
@@ -12,58 +12,81 @@ public class AzulMedioEncoder extends AutoFunction {
         waitForStart();
         if (opModeIsActive()) {
 
+            //score 3 balls
             Catapulta_score();
 
+            //Ré - Load 1
             stopResetMotors();
             autonomous(-1200, -1200, -1200, -1200, 0.8, 0.8, 0, CatapultaModes.HOLD, 1.2);
 
+            //Giro load 1
             stopResetMotors();
             autonomous(-540, 540, -540, 540, 0.7, 0.7, 0, CatapultaModes.HOLD, 0.6);
 
+            //strafe load 1
             stopResetMotors();
             autonomous(-1100, -1100, 1100, 1100, 0.8, 0.8, 0, CatapultaModes.HOLD, 1.2);
 
+            //load 1
             stopResetMotors();
             autonomous(2000, 2000, 2000, 2000, 0.5, 0.5, 0.9, CatapultaModes.HOLD, 2.15);
 
+            // ré - score 6 balls
             stopResetMotors();
             autonomous(-570, -570, -570, -570, 0.8, 0.8, 0.9, CatapultaModes.HOLD, 0.5);
 
+            //strafe score 6 balls
             stopResetMotors();
             autonomous(1770, 1770, -1770, -1770, 0.8, 0.8, 0.9, CatapultaModes.DOWN, 1.15);
 
+            //giro score 6 balls
             stopResetMotors();
             autonomous(600, -600, 600, -600, 0.7, 0.7, 0, CatapultaModes.HOLD, 0.5);
 
+            //parede score 6 balls
             stopResetMotors();
             autonomous(500, 500, 500, 500, 0.8, 0.8, 0, CatapultaModes.HOLD, 0.4);
 
+            //score 6 balls
             Catapulta_score();
-            autonomous(-540, 540, -540, 540, 0.8, 0.8, 0, CatapultaModes.HOLD, 0.4);
 
+
+            //re load 2
             stopResetMotors();
-            autonomous(-600, 600, -600, 600, 0.8, 0.8, 0, CatapultaModes.HOLD, 0.4);
+            autonomous(-1200, -1200, -1200, -1200, 0.8, 0.8, 0, CatapultaModes.HOLD, 1.2);
 
+            //giro load 2
+            stopResetMotors();
+            autonomous(-600, 600, -600, 600, 0.8, 0.8, 0, CatapultaModes.HOLD, 0.5);
+
+            //strafe load 2
             stopResetMotors();
             autonomous(-2100, -2100, 2100, 2100, 0.8, 0.8, 0, CatapultaModes.HOLD, 1.8);
 
+            //load 2
             stopResetMotors();
             autonomous(2200, 2200, 2200, 2200, 0.8, 0.8, 0, CatapultaModes.HOLD, 1.4);
 
+            //ré score 9 balls
             stopResetMotors();
             autonomous(-600, -600, -600, -600, 0.8, 0.8, 0, CatapultaModes.HOLD, 1.4);
 
+            //strafe score 9 balls
             stopResetMotors();
             autonomous(1900, 1900, -1900, -1900, 0.8, 0.8, 0, CatapultaModes.HOLD, 1.4);
 
+            //giro score 9 balls
             stopResetMotors();
             autonomous(600, -600, 600, -600, 0.8, 0.8, 0, CatapultaModes.DOWN, 1.4);
 
+            //parede score 9 balls
             stopResetMotors();
             autonomous(500, 500, 500, 500, 0.8, 0.8, 0, CatapultaModes.DOWN, 1.4);
 
+            //score 9 balls
             Catapulta_score();
-            autonomous(-540, 540, -540, 540, 0.8, 0.8, 0, CatapultaModes.HOLD, 0.4);
+
+            /*autonomous(-540, 540, -540, 540, 0.8, 0.8, 0, CatapultaModes.HOLD, 0.4);
 
             stopResetMotors();
             autonomous(-600, 600, -600, 600, 0.8, 0.8, 0, CatapultaModes.HOLD, 1.4);
@@ -86,7 +109,8 @@ public class AzulMedioEncoder extends AutoFunction {
             stopResetMotors();
             autonomous(500, 500, 500, 500, 0.8, 0.8, 0, CatapultaModes.DOWN, 0.4);
 
-            Catapulta_score();
+
+            Catapulta_score();*/
 
             telemetry.update();
         }
