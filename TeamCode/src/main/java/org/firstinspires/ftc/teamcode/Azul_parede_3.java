f0cfb8b85e0a2b71eb1a660887689842da812c5c
Inácio <inaciotschaefer@gmail.com>
2026-02-21 14:26:13 -0300

    fix: Azul_parede_3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Azul_parede_3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Azul_parede_3.java
@@ -6,14 +6,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous
 public class Azul_parede_3 extends AutoFunction {
 
-    private final ElapsedTime runtime = new ElapsedTime();
-
-    public void waitSeconds(double seconds) {
-        runtime.reset();
-        while (runtime.seconds() <= seconds) {
-        }
-    }
-
     @Override
     public void runOpMode() {
         HardwareMap(hardwareMap);
@@ -21,34 +13,34 @@ public class Azul_parede_3 extends AutoFunction {
         waitForStart();
         if (opModeIsActive()) {
             stopResetMotors();
-            /*autonomous(-1400, -1400, -1400, -1400, 0.8, 0.8, 0, CatapultaModes.DOWN);
-            waitSeconds(3);
+            autonomous( -1400, -1400, -1400, -1400, 0.8, 0.8, 0, CatapultaModes.HOLD, 1);
+            //waitSeconds(3);
 
             stopResetMotors();
-            autonomous(-1300, -1300, 1300, 1300, 0.8, 0.8, 0, CatapultaModes.HOLD);
-            waitSeconds(0);
+            autonomous(-1300, -1300, 1300, 1300, 0.8, 0.8, 0, CatapultaModes.HOLD, 1);
+            //waitSeconds(0);
 
             stopResetMotors();
-            autonomous(410, -410, 410, -410, 0.7, 0.7, 0, CatapultaModes.HOLD);
-            waitSeconds(0);
+            autonomous(410, -410, 410, -410, 0.7, 0.7, 0, CatapultaModes.HOLD,1);
+            //waitSeconds(0);
 
             stopResetMotors();
-            autonomous(2000, 2000, 2000, 2000, 0.8, 0.8, 0, CatapultaModes.HOLD);
-            waitSeconds(0);
+            autonomous(2000, 2000, 2000, 2000, 0.8, 0.8, 0, CatapultaModes.HOLD, 1);
+            //waitSeconds(0);
 
             Catapulta_score();
-            waitSeconds(0);
+            //waitSeconds(0);
 
             stopResetMotors();
-            autonomous(-600,600,-600,600,0.7,0.7,0,CatapultaModes.DOWN);
-            waitSeconds(0);
+            autonomous(-600,600,-600,600,0.7,0.7,0,CatapultaModes.DOWN, 1);
+            //waitSeconds(0);
 
             stopResetMotors();
-            autonomous(-1400,-1400,-1400,-1400,0.8,0.8,0,CatapultaModes.HOLD);
-            waitSeconds(0);
+            autonomous(-1400,-1400,-1400,-1400,0.8,0.8,0,CatapultaModes.HOLD, 1);
+            //waitSeconds(0);
 
             stopResetMotors();
-            autonomous(-5000, -5000, 5000, 5000, 0.8, 0.8, 0, CatapultaModes.HOLD);*/
+            autonomous(-5000, -5000, 5000, 5000, 0.8, 0.8, 0, CatapultaModes.HOLD, 2);
 
             telemetry.update();
         }
